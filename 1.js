0c776c5fb1b15d85be330d8e137b2c58131ae096
rupjitdhali <rupjitdhali@gmail.com>
2025-10-20 15:17:37 +0000

    Javascript day 1

---
+++ 1.js
@@ -0,0 +1,11 @@
+// TO print anyhing in console
+console.log("Hello, World!");
+console.log("I am Rupjit");
+console.log("I am learning JavaScript");
+console.log("I am in Hyderabad");
+console.log("I don't give up");
+
+//Yeh line execute nahi hoti also knows as comments
+/*
+This is an example of multi-line comment
+*/
---
+++ 4.js
@@ -0,0 +1,23 @@
+var standard = 'boy';
+console.log(standard)
+
+var standard = 'Man';
+console.log(standard)
+
+var standard = 'Superman'
+console.log(standard)
+
+let firstName = 'Rupjit'
+let lastName = 'Dhali'
+let age = 19
+let phone = 7005936522
+let favoriteFood = 'Banana'
+let laptopName = 'Ultimus'
+let address = 'New Town Road, Daspara, Udaipur, Gomati Dist, Tripura'
+console.log(firstName)
+console.log(lastName)
+console.log(age)
+console.log(phone)
+console.log(favoriteFood)
+console.log(laptopName)
+console.log(address)
